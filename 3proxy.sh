9ee3368da50255407a411190ca9e29ed03385596
al1kanar <83093510+al1kanar@users.noreply.github.com>
2021-04-23 23:58:19 +0300

    Update 3proxy.sh

--- 3proxy.sh
+++ 3proxy.sh
@@ -1,5 +1,5 @@
 install_3proxy() {
-    echo "installing 3proxy"
+    echo "yükleniyor 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
